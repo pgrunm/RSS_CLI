ac6d9ba1a6263f8190337f1a72ed5981e1d3180e
Warryz <8317771+Warryz@users.noreply.github.com>
2019-12-19 14:37:09 +0100

    Added configuration file.

--- reader.go
+++ reader.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
 func main() {
-	ParseFeeds("https://rss.golem.de/rss.php?feed=RSS2.0", "")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file, %s", err)
+	}
+
+	// Parse configuration
+	feeds := viper.GetStringSlice("Feeds")
+	proxy := viper.GetString("Proxy")
+
+	for _, feed := range feeds {
+		ParseFeeds(feed, proxy)
+	}
+
 }
